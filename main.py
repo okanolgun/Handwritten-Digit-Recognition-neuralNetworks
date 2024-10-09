267f109196df925dd61d6609a4b35bc51817f469
Okan Olgun <98118524+okanolgun@users.noreply.github.com>
2024-10-09 21:52:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,22 +4,31 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import os
 
-# mnist = tf.keras.datasets.mnist
-# (x_train, y_train) , (x_test, y_test) = mnist.load_data()
-#
-# x_train = tf.keras.utils.normalize(x_train, axis=1)
-# x_test = tf.keras.utils.normalize(x_test, axis=1)
-#
-# model = tf.keras.models.Sequential()
-#
-# model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
-# model.add(tf.keras.layers.Dense(128,activation='relu'))
-# model.add(tf.keras.layers.Dense(128,activation='relu'))
-# model.add(tf.keras.layers.Dense(10,activation='softmax'))
-#
-# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-#
-# model.fit(x_train, y_train)
-# model.save('handWrittenModel')
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-model = tf.keras.models.load_model('')
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+model = tf.keras.models.load_model('handWrittenModel')
+
+loss, accuracy = model.evaluate(x_test, y_test)
+
+print(f"Loss: {loss}")
+print(f"Accuracy: {accuracy}")
+
+
+image_number = 1
+while os.path.isfile(f"digits/digit{image_number}.png"):
+    try:
+        img = cv2.imread(f"digits/digit{image_number}.png", cv2.IMREAD_GRAYSCALE)
+        img = np.invert(np.array([img]))
+        prediction = model.predict(img)
+        print(f"This digit is probably a {np.argmax(prediction)}")
+
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        image_number += 1
